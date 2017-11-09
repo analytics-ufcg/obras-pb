e308ebfa60bc8e53b00566351eae7152320774d1
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-11-09 16:27:27 -0300

    Creating endpoint that counts buildings

--- app/controllers/DBController.scala
+++ app/controllers/DBController.scala
@@ -5,6 +5,7 @@ import javax.inject.{Inject, Singleton}
 import play.api.db._
 import play.api.mvc._
 import anorm._
+import anorm.SqlParser._
 import models.Building
 import play.api.libs.json._
 import ai.x.play.json.Jsonx
@@ -13,9 +14,9 @@ import ai.x.play.json.Jsonx
 class DBController @Inject()(dbapi: DBApi) extends Controller {
 
   private val db = dbapi.database("default")
-  val parser: RowParser[Building] = Macro.namedParser[Building]
 
   def getBuildings(limit: Int, offset: Int) = Action {
+    val parser: RowParser[Building] = Macro.namedParser[Building]
     db.withConnection { implicit connection =>
       val result: List[Building] = SQL("SELECT * FROM Obras LIMIT {limit} OFFSET {offset}")
         .on("limit" -> limit, "offset" -> offset)
@@ -26,6 +27,17 @@ class DBController @Inject()(dbapi: DBApi) extends Controller {
       val json = result.map(building => Json.toJson(building))
 
       Ok(Json.toJson(json))
+    }
+  }
+
+  def countBuildings = Action {
+    val rowParser = scalar[Long]
+    db.withConnection { implicit connection =>
+      val result: Int = SQL("SELECT COUNT(*) FROM Obras").as(scalar[Int].single)
+
+      val json = Json.obj("total" -> result)
+
+      Ok(json)
     }
   }
 
