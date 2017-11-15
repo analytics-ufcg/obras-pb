5b9548e71b6d02af7c5f03f1ec5f8d6cf84df8e1
Órion Winter <orion.lima@ccc.ufcg.edu.br>
2017-11-15 15:13:35 -0300

    Seleciona colunas no front-end

--- public/javascripts/controller/HomeController.js
+++ public/javascripts/controller/HomeController.js
@@ -1,17 +1,31 @@
-angular.module('obrasPb').controller('HomeController', ['ObrasService', 'numberOfBuildings', function (ObrasService, numberOfBuildings) {
+angular.module('obrasPb').controller('HomeController', HomeController);
+
+HomeController.$inject = ['ObrasService', 'numberOfBuildings'];
+
+function HomeController(ObrasService, numberOfBuildings) {
     var self = this;
+
+    self.fields = [
+        {name: 'dt_Ano', description: "Ano"},
+        {name: 'de_Localizacao', description: "Local"},
+        {name: 'de_Sucinta', description: "Descrição"},
+        {name: 'vl_Obra', description: "Valor em R$"}
+    ];
 
     const DEFAULT_PAGE_SIZE = 5;
     const DEFAULT_START_PAGE = 1;
+    const SHOWN_FIELDS = self.fields.map(field => field.name);
 
+    var ORDER_BY_FIELD = 'cd_Ugestora';
     this.selectedObras = {};
+
     this.numberOfBuildings = numberOfBuildings;
 
-
-    this.tableQuery = {
-        order : 'dt_Ano',
+    self.tableQuery = {
+        order : ORDER_BY_FIELD,
         limit : DEFAULT_PAGE_SIZE,
-        page : DEFAULT_START_PAGE
+        page : DEFAULT_START_PAGE,
+        fields: SHOWN_FIELDS
     };
 
     this.getStartElement = function (page, limit) {
@@ -21,16 +35,19 @@ angular.module('obrasPb').controller('HomeController', ['ObrasService', 'numberO
     this.getObras = function () {
         self.obrasPromise = ObrasService.getObras(
             self.tableQuery.limit,
-            self.getStartElement(self.tableQuery.page, self.tableQuery.limit)
+            self.getStartElement(self.tableQuery.page, self.tableQuery.limit),
+            self.tableQuery.order,
+            self.tableQuery.fields
         ).then(function (result) {
-            self.obras = result.data.lista;
-        }).
-          catch(function (err) {
-            console.log(err);
-        })
+                self.obras = result.data.lista;
+            }).
+            catch(function (err) {
+                console.log(err);
+            })
     };
 
 
     self.getObras();
 
-}]);
+
+}
