b1f18de545e1b61f303a13448a9170cec21b287c
joeberth <joeberth.souza@ccc.ufcg.edu.br>
2017-11-23 15:51:51 -0300

    Link para o relatorio

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -2,6 +2,7 @@ angular.module('obrasPb').config(['$stateProvider', '$urlRouterProvider', functi
 
     $urlRouterProvider.when("", "/home");
     $urlRouterProvider.when("/", "/home");
+    $urlRouterProvider.when("/report", "/report");
 
     $urlRouterProvider.otherwise("/home");
 
@@ -12,4 +13,10 @@ angular.module('obrasPb').config(['$stateProvider', '$urlRouterProvider', functi
             controller: 'HomeController as homeCtrl'
         });
 
+    $stateProvider
+        .state('report', {
+            url: '/report',
+            templateUrl: '/assets/views/relatorio-inconsistencias.html',
+            controller: 'HomeController as homeCtrl'
+        })
 }]);
