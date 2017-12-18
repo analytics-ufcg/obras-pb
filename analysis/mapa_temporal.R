dba82015b16fbef5592ef7f72e9834118f99166d
Lucas Diniz <lucazdinis@gmail.com>
2017-12-18 17:02:30 -0300

    Refatora rmd com os mapas iterativos e o app shiny para usarem as funcoes do pacote GeoPbUtils

--- analysis/mapa_temporal.R
+++ analysis/mapa_temporal.R
@@ -1,12 +1,17 @@
 library(shiny)
 library(leaflet)
+library(dygraphs)
 library(rgdal)
 library(tidyverse)
-library(RColorBrewer)
-library(dygraphs)
+library(RPostgreSQL)
 #devtools::install_github("analytics-ufcg/geopbutils")
 library(GeoPBUtils)
-library(RPostgreSQL)
+
+
+# Importa tabelas csv
+tipos.das.obras <- read.csv("tipos_obra.csv")
+municipios.pb <- read.csv("municipios_pb.csv")
+mapa_paraiba <- readOGR("mapa_paraiba_ibge/Municipios.shp")
 
 # Pega dados
 drv <- DBI::dbDriver("PostgreSQL")
@@ -27,15 +32,12 @@ con2 <- DBI::dbConnect(drv,
                        dbname = config::get("dbname2")
 )
 
-GeoPBUtils::get.data(con1, con2)
+
+GeoPBUtils::get.data(con1, con2, mapa_paraiba)
 
 dbDisconnect(con1)
 dbDisconnect(con2)
 
-# Importa tabelas restantes
-tipos.das.obras <- read.csv("tipos_obra.csv")
-municipios.pb <- read.csv("municipios_pb.csv")
-mapa_paraiba <- readOGR("mapa_paraiba_ibge/Municipios.shp")
 
 # Manipulação de dados
 municipios <- data.frame(codigo_ibge = mapa_paraiba$GEOCODIG_M, lat = coordinates(mapa_paraiba)[,2], lon = coordinates(mapa_paraiba)[,1])
@@ -43,16 +45,16 @@ municipios <- data.frame(codigo_ibge = mapa_paraiba$GEOCODIG_M, lat = coordinate
 obras.2013 <- get.georreferencia.inputada(obra, localidade, tipos.das.obras, municipios, obra.georref.centroide.sumarizado, 2013) %>% 
     filter(codigo_ibge != 0)
 
-municipios.georref.prop <<- get.prop.municipios.georref(obras.2013, "Não selecionado")
+municipios.georref.porc <<- get.porc.municipios.georref(obras.2013, "Não selecionado")
 
-mapa_paraiba_georreferenciada <- get.mapa.paraiba.georref(mapa_paraiba, municipios.georref.prop)
+mapa_paraiba_georreferenciada <- get.mapa.paraiba.georref(mapa_paraiba, municipios.georref.porc)
 
 # Interface do usuário
 altura.mapa <- 400
 altura.linha.tempo <- 200
 ui <- fluidPage(
     selectInput("select_municipio", label = h3("Selecione o município"), 
-                choices = municipios.georref.prop$nome.x),
+                choices = municipios.georref.porc$nome.x),
     sidebarLayout(  
         sidebarPanel(
             plotOutput("ranking1", height = altura.mapa + altura.linha.tempo)  
@@ -71,17 +73,18 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     v <- reactiveValues(msg = "")
 
-    cores <- paleta.de.cores(dado = mapa_paraiba_georreferenciada@data$prop.georref, reverse = TRUE)
+    cores <- paleta.de.cores(dado = mapa_paraiba_georreferenciada@data$porc.georref, reverse = TRUE)
     
     output$map1 <- renderLeaflet({
         cria.mapa(
             mapa_paraiba_georreferenciada, 
-            mapa_paraiba_georreferenciada@data$prop.georref, 
+            mapa_paraiba_georreferenciada@data$porc.georref, 
             mapa_paraiba_georreferenciada@data$Nome_Munic, 
             get.popup.georref(mapa_paraiba_georreferenciada@data$Nome_Munic, 
                       mapa_paraiba_georreferenciada@data$total.obras, 
                       mapa_paraiba_georreferenciada@data$qtde.georref, 
-                      mapa_paraiba_georreferenciada@data$prop.georref),
+                      mapa_paraiba_georreferenciada@data$porc.georref,
+                      mapa_paraiba_georreferenciada@data$possui.georref.mas.tem.coordenadas.fora.municipio),
             cores, 
             "Proporção de obras</br> georreferenciadas em %",
             mapa_paraiba_georreferenciada@data$cor.borda,
@@ -96,15 +99,17 @@ server <- function(input, output, session) {
             summarise(
                 total.obras = n(),
                 qtde.georref = sum(!is.inputado),
-                prop.georref = (qtde.georref / total.obras) * 100
+                porc.georref = (qtde.georref / total.obras) * 100
             ) %>% 
-            select(ano, prop.georref) %>% 
+            select(ano, porc.georref) %>% 
             dygraph() %>% 
             dyRangeSelector() %>%
             dyLegend(show = "never")
     })
     
-    output$ranking1 <- plot.ranking(municipios.georref.prop, input$select_municipio)
+    output$ranking1 <- renderPlot({
+        plot.ranking(municipios.georref.porc, input$select_municipio)
+    })
     
     observeEvent({
         input$select_municipio
@@ -121,29 +126,32 @@ server <- function(input, output, session) {
                 ano.final <<- ano2
                 municipio.selecionado <<- municipio
                 
-                municipios.georref.prop <<- get.prop.municipios.georref(obras.2013, municipio.selecionado, ano.inicial, ano.final)
+                municipios.georref.porc <<- get.porc.municipios.georref(obras.2013, municipio.selecionado, ano.inicial, ano.final)
                 
-                updateSelectInput(session, inputId = "municipio", choices = municipios.georref.prop$nome.x)
+                updateSelectInput(session, inputId = "municipio", choices = municipios.georref.porc$nome.x)
                 
-                mapa_paraiba_georreferenciada <- get.mapa.paraiba.georref(mapa_paraiba, municipios.georref.prop)
+                mapa_paraiba_georreferenciada <- get.mapa.paraiba.georref(mapa_paraiba, municipios.georref.porc)
                 
-                cores <- paleta.de.cores(dado = mapa_paraiba_georreferenciada@data$prop.georref, reverse = TRUE)
+                cores <- paleta.de.cores(dado = mapa_paraiba_georreferenciada@data$porc.georref, reverse = TRUE)
                 
                 leafletProxy("map1", data = mapa_paraiba_georreferenciada) %>%
                     clearGroup( group = "municipios-poligono" ) %>%
                     clearControls() %>%
                     adiciona.poligonos.e.legenda(cores,
-                                                 mapa_paraiba_georreferenciada@data$prop.georref, 
+                                                 mapa_paraiba_georreferenciada@data$porc.georref, 
                                                  mapa_paraiba_georreferenciada@data$Nome_Munic, 
                                                  get.popup.georref(mapa_paraiba_georreferenciada@data$Nome_Munic, 
                                                            mapa_paraiba_georreferenciada@data$total.obras, 
                                                            mapa_paraiba_georreferenciada@data$qtde.georref, 
-                                                           mapa_paraiba_georreferenciada@data$prop.georref),
+                                                           mapa_paraiba_georreferenciada@data$porc.georref,
+                                                           mapa_paraiba_georreferenciada@data$possui.georref.mas.tem.coordenadas.fora.municipio),
                                                  "Proporção de obras</br> georreferenciadas em %",
                                                  mapa_paraiba_georreferenciada@data$cor.borda,
                                                  mapa_paraiba_georreferenciada@data$largura.borda)
                 
-                output$ranking1 <- plot.ranking(municipios.georref.prop, input$select_municipio)
+                output$ranking1 <- renderPlot({
+                    plot.ranking(municipios.georref.porc, input$select_municipio)
+                })
             }
         }
     })
