e1230eef1011c0b027582146e0ad6cb97aadb280
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-12-20 14:47:24 -0300

    Modifica host para funcionar na VM

--- analysis/run_shiny.R
+++ analysis/run_shiny.R
@@ -1,2 +1,2 @@
 library(shiny)
-runApp("mapa_temporal.R", port = 9090, launch.browser = FALSE, test.mode = FALSE)
+runApp("mapa_temporal.R", port = 9090, host = "0.0.0.0", launch.browser = FALSE, test.mode = FALSE)
